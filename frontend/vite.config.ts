5ba2917c030644923d60c565b224728095d68d4c
Corey Mead <corey@Coreys-MacBook-Pro.local>
2025-04-20 21:40:11 -0400

    adding 404 route

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/zoom-class-builder/'
+  base: '/zoom-class-builder/',
+  server: {
+    port: 5173,
+    strictPort: true
+  }
 }) 
